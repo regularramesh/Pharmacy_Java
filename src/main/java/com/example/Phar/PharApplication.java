421646861c4b7716caa01eaddd7e63c58ccb5a2d
rameshmadhesh <mail2rameshmadhesh@gmail.com>
2024-05-02 13:25:57 +0530

    first

---
+++ src/main/java/com/example/Phar/PharApplication.java
@@ -0,0 +1,13 @@
+package com.example.Phar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharApplication.class, args);
+	}
+
+}
